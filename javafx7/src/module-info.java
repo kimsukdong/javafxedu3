31ca1ef49da99bb0b8bede24a47bc934b6be64e4
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-10-26 22:06:36 +0900

    add manager

---
+++ javafx7/src/module-info.java
@@ -0,0 +1,8 @@
+module javafx7 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	
+	opens application to javafx.graphics, javafx.fxml,javafx.base;
+}
