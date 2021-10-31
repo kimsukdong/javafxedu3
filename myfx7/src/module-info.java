e711524de725f4ff253e5afdd0159e7b03b64a28
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-10-31 20:13:27 +0900

    total project

---
+++ myfx7/src/module-info.java
@@ -0,0 +1,9 @@
+module myfx7 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	
+	opens phone to javafx.graphics, javafx.fxml,javafx.base;
+	opens phone.controller to javafx.graphics, javafx.fxml,javafx.base;
+}
